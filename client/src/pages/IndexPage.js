b9716f71a6777ff14571ea77bb3b7a4a2c0dba1a
Paulongf <paulongf@hotmail.com>
2025-04-21 19:15:03 +0100

    retornando as urls

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -5,7 +5,7 @@ import {useEffect, useState} from "react";
 export default function IndexPage() {
   const [posts, setPosts] = useState([]);
   useEffect(()=>{
-    fetch('https://palmeiras-porto-frontend.onrender.com/post').then(response =>{
+    fetch('http://localhost:4000/post').then(response =>{
       response.json().then(posts =>{
         setPosts(posts);
       })
