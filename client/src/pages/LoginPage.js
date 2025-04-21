af83797846fa2caf706750f218cc3336fbe91776
Paulongf <paulongf@hotmail.com>
2025-04-21 19:29:21 +0100

    retornando as urls

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -12,7 +12,7 @@ export default function LoginPage(){
     async function login(ev) {
         ev.preventDefault();
         try {
-            const response = await fetch('https://palmeirasporto-backend.onrender.com/login', {
+            const response = await fetch('http://localhost:4000/login', {
                 method: 'POST',
                 body: JSON.stringify({ username, password }),
                 headers: { 'Content-Type': 'application/json' },
